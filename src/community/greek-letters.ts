8c751900273156c17147ff369ab4e2416e802039
Dan Simon <dan@pipeline.com>
2019-12-07 19:17:17 -0500

    try to get rid of the crlf, also some other easy fixes, still lots of hard fixes remain

--- src/community/greek-letters.ts
+++ src/community/greek-letters.ts
@@ -1,31 +1,26 @@
 import { Notation } from "../notation";
 import Decimal from "break_infinity.js/break_infinity";
 
-export class GreekLettersNotation extends Notation
-{
-    public get name(): string
-    {
-        return "Greek Letters";
-    }
+export class GreekLettersNotation extends Notation {
+  public get name(): string {
+    return "Greek Letters";
+  }
 
-    private get greek(): string[]
-    {
-        return "άαβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ".split("");
-    }
+  private get greek(): string[] {
+    return "άαβγδεζηθικλμνξοπρστυφχψωΑΒΓΔΕΖΗΘΙΚΛΜΝΞΟΠΡΣΤΥΦΧΨΩ".split("");
+  }
 
-    formatDecimal(value: Decimal, places: number): string
-    {
-        let exp = Math.floor(value.e / 3);
-        let step = Math.pow(this.greek.length, Math.floor(Math.log(exp) / Math.log(this.greek.length)));
-        let suffix = "";
-        while(step >= 1)
-        {
-            let ordinal = Math.floor(exp / step);
-            suffix += this.greek[ordinal];
-            exp -= step * ordinal;
-            step /= this.greek.length;
-        }
-        let mantissa = Decimal.pow(10, Decimal.log10(value) % 3).toFixed(places);
-        return mantissa + " " + suffix;
+  public formatDecimal(value: Decimal, places: number): string {
+    let exp = Math.floor(value.e / 3);
+    let step = Math.pow(this.greek.length, Math.floor(Math.log(exp) / Math.log(this.greek.length)));
+    let suffix = "";
+    while (step >= 1) {
+      let ordinal = Math.floor(exp / step);
+      suffix += this.greek[ordinal];
+      exp -= step * ordinal;
+      step /= this.greek.length;
     }
+    let mantissa = Decimal.pow(10, Decimal.log10(value) % 3).toFixed(places);
+    return mantissa + " " + suffix;
+  }
 }
