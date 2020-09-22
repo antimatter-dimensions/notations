bac9fd912fd3436a0d77377c601eab5bff27b8c3
Dan Simon <des480@nyu.edu>
2020-09-22 18:22:55 -0400

    Add comment

--- src/community/infix-short-scale.ts
+++ src/community/infix-short-scale.ts
@@ -1,6 +1,7 @@
 import { AbstractInfixNotation } from "./infix-abstract";
 import { toSubscript, abbreviate } from "../utils";
 
+// Name comes from https://en.wikipedia.org/wiki/Long_and_short_scales
 export class InfixShortScaleNotation extends AbstractInfixNotation {
   public get name(): string {
     return "Infix short scale";
