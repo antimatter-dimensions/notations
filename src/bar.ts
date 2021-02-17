da5d0c4069b37ff565236e0215eea2ec0fa1cd0e
earth <divine.icbm@gmail.com>
2021-02-17 16:20:32 -0600

    make bar work under 1000
    big fan of pain

--- src/bar.ts
+++ src/bar.ts
@@ -7,8 +7,36 @@ const LOG8 = Math.log(8);
 export class BarNotation extends Notation {
   public get name(): string {
     return "Bar";
-  }
+  };
+  
+  public get negativeInfinite(): string {
+    return "";
+  };
 
+  public get Infinite(): string {
+    return "";
+  };
+  
+  public formatVerySmallNegativeDecimal(value: Decimal): string {
+    return this.formatDecimal(value.negate());
+  };
+
+  public formatVerySmallDecimal(value: Decimal): string {
+    return this.formatDecimal(value);
+  };
+
+  public formatNegativeUnder1000(value: number): string {
+    return this.formatDecimal(new Decimal(-value));
+  };
+
+  public formatUnder1000(value: number): string {
+    return this.formatDecimal(new Decimal(value));
+  };
+
+  public formatNegativeDecimal(value: Decimal): string {
+    return this.formatDecimal(value.negate());
+  };
+  
   public formatDecimal(value: Decimal): string {
     const log8 = Math.LN10 / LOG8 * value.log10();
     let wholeLog = Math.floor(log8);
@@ -22,8 +50,8 @@ export class BarNotation extends Notation {
       const remainder = wholeLog % 8;
       wholeLog = (wholeLog - remainder) / 8;
       parts.push(BARS[remainder]);
-    }
+    };
     parts.push(BARS[wholeLog]);
     return parts.join("");
-  }
+  };
 }
