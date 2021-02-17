95a5ef27e47d33e4895955f680a2b8838aa7b789
earth <divine.icbm@gmail.com>
2021-02-17 16:36:13 -0600

    remove semis
    apparently codacy gets pissed if there's semis

--- src/bar.ts
+++ src/bar.ts
@@ -7,35 +7,35 @@ const LOG8 = Math.log(8);
 export class BarNotation extends Notation {
   public get name(): string {
     return "Bar";
-  };
+  }
   
   public get negativeInfinite(): string {
     return "-";
-  };
+  }
 
   public get Infinite(): string {
     return "";
-  };
+  }
   
   public formatVerySmallNegativeDecimal(value: Decimal): string {
     return this.formatDecimal(value.negate());
-  };
+  }
 
   public formatVerySmallDecimal(value: Decimal): string {
     return this.formatDecimal(value);
-  };
+  }
 
   public formatNegativeUnder1000(value: number): string {
     return this.formatDecimal(new Decimal(-value));
-  };
+  }
 
   public formatUnder1000(value: number): string {
     return this.formatDecimal(new Decimal(value));
-  };
+  }
 
   public formatNegativeDecimal(value: Decimal): string {
     return this.formatDecimal(value.negate());
-  };
+  }
   
   public formatDecimal(value: Decimal): string {
     const log8 = Math.LN10 / LOG8 * value.log10();
@@ -53,5 +53,5 @@ export class BarNotation extends Notation {
     };
     parts.push(BARS[wholeLog]);
     return parts.join("");
-  };
+  }
 }
