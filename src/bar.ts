5f0f6d0309650ee70407d23bcbd9cf57ab964e95
earth <divine.icbm@gmail.com>
2021-03-23 09:56:02 -0500

    Update bar.ts

--- src/bar.ts
+++ src/bar.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Notation } from "./notation";
 import Decimal from "break_infinity.js";
 
@@ -73,34 +72,3 @@ export class BarNotation extends Notation {
     return newParts.join("");
   }
 }
-=======
-import { Notation } from "./notation";
-import Decimal from "break_infinity.js/break_infinity";
-
-const BARS = ["", "", "", "", "", "", "", ""];
-const LOG8 = Math.log(8);
-
-export class BarNotation extends Notation {
-  public get name(): string {
-    return "Bar";
-  }
-
-  public formatDecimal(value: Decimal): string {
-    const log8 = Math.LN10 / LOG8 * value.log10();
-    let wholeLog = Math.floor(log8);
-    const decimalLog = log8 - wholeLog;
-    const decimalLog64 = Math.floor(decimalLog * 64);
-    const parts = [
-      BARS[decimalLog64 % 8],
-      BARS[Math.floor(decimalLog64 / 8)]
-    ];
-    while (wholeLog >= 8) {
-      const remainder = wholeLog % 8;
-      wholeLog = (wholeLog - remainder) / 8;
-      parts.push(BARS[remainder]);
-    }
-    parts.push(BARS[wholeLog]);
-    return parts.join("");
-  }
-}
->>>>>>> 5a7ca1fd8bc6f0171d78f826a4c8baf6690d6f25
