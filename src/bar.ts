cbd56ad31e992d44b63e9ff19c82da8839d163e7
earth <divine.icbm@gmail.com>
2021-03-23 09:54:27 -0500

    make negative stuff
    aaaaaa

--- src/bar.ts
+++ src/bar.ts
@@ -1,7 +1,12 @@
+<<<<<<< HEAD
 import { Notation } from "./notation";
 import Decimal from "break_infinity.js";
 
+// The reason these have to be these unicode boxes and not their escape characters 
+// is beyond me. However, you can trust that these will render correctly, as they
+// are part of the font found in docs/MonospaceTypewriter.ttf
 const BARS = ["", "", "", "", "", "", "", ""];
+const NEGATIVE_BARS = ["", "", "", "", "", "", "", ""];
 const LOG8 = Math.log(8);
 
 export class BarNotation extends Notation {
@@ -10,7 +15,7 @@ export class BarNotation extends Notation {
   }
   
   public get negativeInfinite(): string {
-    return "-";
+    return "";
   }
 
   public get Infinite(): string {
@@ -18,7 +23,7 @@ export class BarNotation extends Notation {
   }
   
   public formatVerySmallNegativeDecimal(value: Decimal): string {
-    return '-' + this.formatDecimal(value.negate());
+    return this.flipBars(this.formatDecimal(value));
   }
 
   public formatVerySmallDecimal(value: Decimal): string {
@@ -26,7 +31,7 @@ export class BarNotation extends Notation {
   }
 
   public formatNegativeUnder1000(value: number): string {
-    return '-' + this.formatDecimal(new Decimal(-value));
+    return this.flipBars(this.formatDecimal(new Decimal(value)));
   }
 
   public formatUnder1000(value: number): string {
@@ -34,16 +39,53 @@ export class BarNotation extends Notation {
   }
 
   public formatNegativeDecimal(value: Decimal): string {
-    return '-' + this.formatDecimal(value.negate());
+    return this.flipBars(this.formatDecimal(value));
   }
   
   public formatDecimal(value: Decimal): string {
     if (value.eq(0)) {
       return '0';
     }
-    if (value.lessThan(1)) {
+    if (value.lessThan(1) && value.greaterThan(0)) {
       return '/' + this.formatDecimal(Decimal.div(1, value));
     }
+    const log8 = Math.LN10 / LOG8 * value.log10();
+    let wholeLog = Math.floor(log8);
+    const decimalLog = log8 - wholeLog;
+    const decimalLog64 = Math.floor(decimalLog * 64);
+      const parts = [
+        BARS[decimalLog64 % 8],
+        BARS[Math.floor(decimalLog64 / 8)]
+      ];
+      while (wholeLog >= 8) {
+        const remainder = wholeLog % 8;
+        wholeLog = (wholeLog - remainder) / 8;
+        parts.push(BARS[remainder]);
+      }
+      parts.push(BARS[wholeLog]);
+      return parts.join("");
+  }
+  public flipBars(parts: string): string {
+    let newParts = [];
+    for (let i = 0; i < parts.length; i++) {
+      newParts.push(NEGATIVE_BARS[BARS.indexOf(parts[i])])
+    }
+    return newParts.join("");
+  }
+}
+=======
+import { Notation } from "./notation";
+import Decimal from "break_infinity.js/break_infinity";
+
+const BARS = ["", "", "", "", "", "", "", ""];
+const LOG8 = Math.log(8);
+
+export class BarNotation extends Notation {
+  public get name(): string {
+    return "Bar";
+  }
+
+  public formatDecimal(value: Decimal): string {
     const log8 = Math.LN10 / LOG8 * value.log10();
     let wholeLog = Math.floor(log8);
     const decimalLog = log8 - wholeLog;
@@ -61,3 +103,4 @@ export class BarNotation extends Notation {
     return parts.join("");
   }
 }
+>>>>>>> 5a7ca1fd8bc6f0171d78f826a4c8baf6690d6f25
