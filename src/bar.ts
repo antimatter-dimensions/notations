60f04950b325c7acf31c4005910a4c702b237b18
earth <divine.icbm@gmail.com>
2021-03-23 21:23:27 -0500

    stuff
    semi and indentation fixes

--- src/bar.ts
+++ src/bar.ts
@@ -52,22 +52,22 @@ export class BarNotation extends Notation {
     let wholeLog = Math.floor(log8);
     const decimalLog = log8 - wholeLog;
     const decimalLog64 = Math.floor(decimalLog * 64);
-      const parts = [
-        BARS[decimalLog64 % 8],
-        BARS[Math.floor(decimalLog64 / 8)]
-      ];
-      while (wholeLog >= 8) {
-        const remainder = wholeLog % 8;
-        wholeLog = (wholeLog - remainder) / 8;
-        parts.push(BARS[remainder]);
-      }
-      parts.push(BARS[wholeLog]);
-      return parts.join("");
+    const parts = [
+      BARS[decimalLog64 % 8],
+      BARS[Math.floor(decimalLog64 / 8)]
+    ];
+    while (wholeLog >= 8) {
+      const remainder = wholeLog % 8;
+      wholeLog = (wholeLog - remainder) / 8;
+      parts.push(BARS[remainder]);
+    }
+    parts.push(BARS[wholeLog]);
+    return parts.join("");
   }
   public flipBars(parts: string): string {
     const newParts = [];
     for (const part of parts) {
-      newParts.push(NEGATIVE_BARS[BARS.indexOf(part)])
+      newParts.push(NEGATIVE_BARS[BARS.indexOf(part)]);
     }
     return newParts.join("");
   }
