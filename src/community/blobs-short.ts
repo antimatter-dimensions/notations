5eb366b4688849b0810258069a60932bcd482f9f
Buck4437 <Buck4437@users.noreply.github.com>
2021-10-13 18:01:22 +0800

    bug fix + a bit of refactor

--- src/community/blobs-short.ts
+++ src/community/blobs-short.ts
@@ -1,43 +1,34 @@
 import { BlobsNotation } from "./blobs";
 import Decimal from "break_infinity.js";
 
-const EMPTY = "";
-const NEGATIVE = "un";
-const INFINITY = "finity";
-const SUFFIXES = [EMPTY, "think", "wave", "hug", "nom", "sad", "pats", "yes", "no", "heart", "sleep"];
-
 export class BlobsShortNotation extends BlobsNotation {
   public get name(): string {
     return "Blobs (Short)";
   }
 
-  public get negativeInfinite(): string {
-    return this.blobConstructor(NEGATIVE, INFINITY);
-  }
-
-  public get infinite(): string {
-    return this.blobConstructor(EMPTY, INFINITY);
+  protected get prefix_negative(): string {
+    return "un";
   }
 
   protected blobify(num: Decimal): string {
     let prefix = "", suffix = "";
     let number = this.reduceNumber(num.abs());
     if (num.sign() === -1) {
-      prefix = NEGATIVE;
+      prefix = this.prefix_negative;
       // To allow the combination :notlikeblob: to appear
       number = Math.max(0, number - 1);
     }
 
     const indexes = [0, 0];
 
-    indexes[1] = number % SUFFIXES.length;
-    indexes[0] = (number - indexes[1]) / SUFFIXES.length;
+    indexes[1] = number % this.suffixes.length;
+    indexes[0] = (number - indexes[1]) / this.suffixes.length;
 
     if (indexes[0] >= 1) {
-      suffix = `-${indexes[0] - 1}`;
+      suffix = `-${indexes[0] + 1}`;
     }
 
-    return this.blobConstructor(prefix, SUFFIXES[Math.floor(indexes[1])] + suffix);
+    return this.blobConstructor(prefix, this.suffixes[Math.floor(indexes[1])] + suffix);
   }
-  
+
 }
--- src/community/blobs.ts
+++ src/community/blobs.ts
@@ -2,23 +2,34 @@ import { Notation } from "../notation";
 import Decimal from "break_infinity.js";
 
 const LOG3 = Math.log10(3);
-const EMPTY = "";
-const NEGATIVE = "notlike";
-const INFINITY = "finity";
-const PREFIXES = [EMPTY, "big", "large", "great", "grand", "huge", "super", "ultra", "mega", "giga", "omega"];
-const SUFFIXES = [EMPTY, "think", "wave", "hug", "nom", "sad", "pats", "yes", "no", "heart", "sleep"];
 
 export class BlobsNotation extends Notation {
   public get name(): string {
     return "Blobs";
   }
 
+  protected get prefixes(): Array<string> {
+    return ["", "big", "large", "great", "grand", "huge", "super", "ultra", "mega", "giga", "omega"];
+  }
+
+  protected get suffixes(): Array<string> {
+    return ["", "think", "wave", "hug", "nom", "sad", "pats", "yes", "no", "heart", "sleep"];
+  }
+
+  protected get prefix_negative(): string {
+    return "notlike";
+  }
+
+  protected get suffix_infinity(): string {
+    return "finity";
+  }
+
   public get negativeInfinite(): string {
-    return this.blobConstructor(NEGATIVE, INFINITY);
+    return this.blobConstructor(this.prefix_negative, this.suffix_infinity);
   }
 
   public get infinite(): string {
-    return this.blobConstructor(EMPTY, INFINITY);
+    return this.blobConstructor("", this.suffix_infinity);
   }
 
   public formatVerySmallNegativeDecimal(num: Decimal): string {
@@ -45,24 +56,24 @@ export class BlobsNotation extends Notation {
     let prefix = "", suffix = "";
     let number = this.reduceNumber(num.abs());
     if (num.sign() === -1) {
-      prefix = NEGATIVE;
+      prefix = this.prefix_negative;
       // To allow the combination :notlikeblob: to appear
       number = Math.max(0, number - 1);
     }
 
     const indexes = [0, 0, 0];
 
-    indexes[2] = number % SUFFIXES.length;
-    number = (number - indexes[2]) / SUFFIXES.length;
-    indexes[1] = number % PREFIXES.length;
-    indexes[0] = (number - indexes[1]) / PREFIXES.length;
+    indexes[2] = number % this.suffixes.length;
+    number = (number - indexes[2]) / this.suffixes.length;
+    indexes[1] = number % this.prefixes.length;
+    indexes[0] = (number - indexes[1]) / this.prefixes.length;
 
     if (indexes[0] >= 1) {
-      suffix = `-${indexes[0] - 1}`;
+      suffix = `-${indexes[0] + 1}`;
     }
 
-    return this.blobConstructor(prefix + PREFIXES[Math.floor(indexes[1])],
-                                SUFFIXES[Math.floor(indexes[2])] + suffix);
+    return this.blobConstructor(prefix + this.prefixes[Math.floor(indexes[1])],
+                                this.suffixes[Math.floor(indexes[2])] + suffix);
   }
 
   protected reduceNumber(num: Decimal): number {
